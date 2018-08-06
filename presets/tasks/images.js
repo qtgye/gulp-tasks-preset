d5832d3aa37b86ad33f77d4ab5e566a7f5a4b6b4
Jace Buquia <buquia.jace@gmail.com>
2018-08-06 17:51:50 +0800

    Images task

--- presets/tasks/images.js
+++ presets/tasks/images.js
@@ -1,20 +1,24 @@
-let { isDevelopment, isLocal, isStaging, isProduction, isWatching,
-      projectRoot, browserSync } = require('gulp-tasks-preset');
-
-let imagemin = require('gulp-imagemin');
 let gulp = require('gulp');
-
-let src = projectRoot('source/images/**/*.*');
+let imagemin = require('gulp-imagemin');
+let src = 'src/images/**/*.*';
 let dest = 'dist/images';
 
 module.exports = {
 
-  fn: async function () {
+
+  name: 'images',
+
+
+  description: 'Optimizes images',
+
+
+  task: function () {
     return gulp.src(src)
               .pipe(imagemin())
               .pipe(gulp.dest(dest));
   },
 
-  watchFiles: src,
+
+  watch: src,
 
 }
