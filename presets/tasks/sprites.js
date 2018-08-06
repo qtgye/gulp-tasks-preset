b693ae1c28ab15586f1d65fe356e013561fff29c
Jace Buquia <buquia.jace@gmail.com>
2018-08-06 17:41:38 +0800

    Sprites task

--- presets/tasks/sprites.js
+++ presets/tasks/sprites.js
@@ -1,16 +1,16 @@
-let { isDevelopment, isLocal, isStaging, isProduction, isWatching,
-      projectRoot, browserSync } = require('gulp-tasks-preset');
-
 let svgSprite = require('gulp-svg-sprite');
 let imagemin = require('gulp-imagemin');
+let through = require('through2');
 let gulp = require('gulp');
 
-let src = projectRoot('source/images/icons/**/*.svg');
-let dest = 'dist'
+let src = 'src/icons/**/*.svg';
+let dest = 'dist/images';
 
 module.exports = {
 
-  fn: async function () {
+  name: 'sprites',
+
+  task: () => {
     return gulp.src(src)
             .pipe(imagemin())
             .pipe(svgSprite({
@@ -19,11 +19,17 @@ module.exports = {
               },
               preview: false,
             }))
+            // Ensure that the output file will not have the extra 'symbol/svg/' folder tree made by the svg-sprite plugin
+            .pipe(through.obj(function (file, enc, next) {
+              file.path = file.path.replace('symbol/svg/', '');
+              this.push(file);
+              next();
+            }))
             .pipe(gulp.dest(dest));
   },
 
 
-  watchFiles: src,
+  watch: src,
 
 
 }
